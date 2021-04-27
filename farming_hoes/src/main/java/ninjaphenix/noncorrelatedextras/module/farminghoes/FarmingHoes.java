ced5ccddecc8b4dca47228b77dfd8033ab383dfd
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-04-27 14:22:17 +0100

    Update farming hoes impl, avoids passing null values.

--- farming_hoes/src/main/java/ninjaphenix/noncorrelatedextras/module/farminghoes/FarmingHoes.java
+++ farming_hoes/src/main/java/ninjaphenix/noncorrelatedextras/module/farminghoes/FarmingHoes.java
@@ -8,30 +8,26 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.CactusBlock;
-import net.minecraft.world.level.block.CarvedPumpkinBlock;
 import net.minecraft.world.level.block.CocoaBlock;
 import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.MelonBlock;
-import net.minecraft.world.level.block.PumpkinBlock;
 import net.minecraft.world.level.block.SugarCaneBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class FarmingHoes implements ClientModInitializer {
-    // todo-norelease: rework for 1.16 hoe effective tool change.
     @Override
     public void onInitializeClient() {
-        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+        AttackBlockCallback.EVENT.register((player, level, hand, pos, direction) -> {
             final ItemStack handStack = player.getItemInHand(hand);
             if (handStack.getItem() instanceof HoeItem) {
-                final BlockState state = world.getBlockState(pos);
+                final BlockState state = level.getBlockState(pos);
                 final Block block = state.getBlock();
                 if (block instanceof CropBlock || block instanceof CocoaBlock) {
                     final BonemealableBlock growableBlock = (BonemealableBlock) block;
-                    if (growableBlock.isValidBonemealTarget(null, null, state, false)) {
+                    if (growableBlock.isValidBonemealTarget(level, pos, state, level.isClientSide())) {
                         return InteractionResult.FAIL;
                     }
                 } else if (block instanceof CactusBlock || block instanceof SugarCaneBlock) {
-                    if (world.getBlockState(pos.below()).getBlock() != block) {
+                    if (level.getBlockState(pos.below()).getBlock() != block) {
                         return InteractionResult.FAIL;
                     }
                 }
